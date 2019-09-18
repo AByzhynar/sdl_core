fa2bc4671d3a1f2f24538e3fb92431a9d4e04033
Andriy Byzhynar <abyzhynar@luxoft.com>
2019-09-18 17:57:01 +0300

    Update unit tests according to code changes

--- src/components/include/test/transport_manager/mock_transport_manager.h
+++ src/components/include/test/transport_manager/mock_transport_manager.h
@@ -81,8 +81,9 @@ class MockTransportManager : public ::transport_manager::TransportManager,
   MOCK_METHOD1(AddEventListener, int(TransportManagerListener* listener));
   MOCK_METHOD0(Stop, int());
   MOCK_METHOD1(RemoveDevice, int(const DeviceHandle));
-  MOCK_CONST_METHOD1(Visibility, int(const bool&));
-  MOCK_CONST_METHOD1(EnableClientsListening, int(const bool&));
+  MOCK_CONST_METHOD1(PerformActionOnClients,
+                     int(transport_manager::TransportAction required_action));
+
   MOCK_METHOD1(SetTelemetryObserver,
                void(transport_manager::TMTelemetryObserver* observer));
 };
